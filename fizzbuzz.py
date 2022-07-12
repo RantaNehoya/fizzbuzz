f684773fffa43f37d7fc297b6dde6e6e129e2170
RantaNehoya <nehoya08@gmail.com>
2022-07-12 09:15:11 +0200

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,31 @@
+"""
+fizzbuzz
+"""
+
+
+def fizzbuzz(x: int) -> str:
+    """Returns fizz if arg is divisible by 3, buzz if arg is divisible by 5 and fizzbuzz if divisible by 3 and 5.
+
+    Args:
+        x (int): integer input
+
+    Returns:
+        str: fizz, buzz, or fizzbuzz
+    """
+
+    if x % 3 == 0 and x % 5 == 0:
+        return "fizzbuzz"
+
+    elif x % 3 == 0:
+        return "fizz"
+
+    elif x % 5 == 0:
+        return "buzz"
+
+    else:
+        return str(x)
+
+
+user_input = int(input("Enter a number: "))
+
+print(fizzbuzz(user_input))
